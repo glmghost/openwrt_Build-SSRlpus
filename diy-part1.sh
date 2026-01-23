6a6aa82427d8b65d9c842ce566786b69b8a54aed
glmghost <62704244+glmghost@users.noreply.github.com>
2026-01-23 21:10:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+# Add third-party feed sources before updating feeds
 #
 
-# Add feed sources
+# Add helloworld (required for SSR/V2Ray plugins)
 echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
-# 取消注释下面这行如果你需要 passwall（注意：passwall 依赖 rust/sing-box/xray 等）
+
+# Optional: uncomment if you need PassWall
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;main' >> feeds.conf.default
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages;main' >> feeds.conf.default
