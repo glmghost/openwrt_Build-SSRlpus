0fc5bcc8ad5208140b4753994a2498e1261cd38f
glmghost <62704244+glmghost@users.noreply.github.com>
2026-01-25 19:40:42 +0800

    Update and rename diy-part2.sh to customize.sh

--- diy-part2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Fix for Rust build issues
-sed -i '/CONFIG_TARGET_ROOTFS_INITRAMFS/d' .config
-echo 'CONFIG_TARGET_ROOTFS_INITRAMFS=y' >> .config
-
-# Set default LAN IP to 192.168.31.238
-sed -i 's/192.168.1.1/192.168.31.238/g' package/base-files/files/bin/config_generate
